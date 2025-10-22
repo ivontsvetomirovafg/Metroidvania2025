d0f7b83b49246915b602ad14c9db9beed8a77595
ivontsvetomirovafg <ivontsvetomirova.fg@gmail.com>
2025-10-22 12:01:37 +0200

    Level1Update

---
+++ Assets/Scripts/CamaraController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CamaraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+    [SerializeField]
+    private Vector3 camOffset;
+    [SerializeField]
+    private float minX, maxX, minY, maxY;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float x = Mathf.Clamp(player.position.x,minX,maxX);
+        float y = Mathf.Clamp(player.position.y, minY, maxY);
+
+        transform.position = new Vector3(x, y, camOffset.z);
+    }
+}
--- Assets/Scripts/CharacterControler.cs
+++ Assets/Scripts/CharacterControler.cs
@@ -6,19 +6,27 @@ public class CharacterControler : MonoBehaviour
     private float speed;
     private Rigidbody2D rb;
     private Animator animator;
+    private int jumpCount;
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private float groundDistance;
+
+    //temp
+    private int maxJumps = 1;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        rb.linearVelocity = speed * Vector2.right * horizontal;
+        rb.linearVelocity = new Vector2(speed * horizontal, rb.linearVelocity.y);
 
         if(horizontal == 0)
         {
@@ -35,6 +43,34 @@ public class CharacterControler : MonoBehaviour
         else if(horizontal > 0)
         {
             transform.eulerAngles = Vector3.zero;
+        }
+        if(Input.GetButtonDown("Jump") && jumpCount<maxJumps)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            jumpCount++;
+        }
+
+        Collider2D[] coliders = Physics2D.OverlapCircleAll(transform.position, groundDistance);
+        bool isGrounded = false;
+        for (int i = 0; i<coliders.Length; i++)
+        {
+            if (coliders[i].transform.tag=="Ground")
+            {
+                isGrounded = true;
+            }
+        }
+        if(isGrounded == true)
+        {
+            jumpCount = 0;
+            animator.SetBool("Jump", false);
+        }
+        else
+        {
+            animator.SetBool("Jump", true);
+            if (jumpCount == 0)
+            {
+                jumpCount++;
+            }
         }
     }
     
